231e1c53c7b4129024c92a0fa86b56fb86dd42ae
AngeloMassaro <angeloemme80@alice.it>
2021-02-16 15:46:53 +0100

    padre

---
+++ src/main/java/it/angelomassaro/Padre.java
@@ -0,0 +1,28 @@
+package it.angelomassaro;
+
+import javax.enterprise.context.RequestScoped;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@RequestScoped
+@Path("/padre")
+public class Padre {
+
+	@GET
+	@Path("/primo")
+	@Produces(MediaType.APPLICATION_JSON)
+	public JsonObject mioServizio() {
+		JsonObjectBuilder builder = Json.createObjectBuilder();
+		return builder.add("nome", "Angelo")
+				.add("cognome", "Massaro")
+				.add("eta", 41)
+				.add("timestamp", System.currentTimeMillis())
+                .build();
+	}
+	
+}
